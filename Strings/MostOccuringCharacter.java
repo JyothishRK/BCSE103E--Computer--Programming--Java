2c8802fc48f54fd343af698b6b4aafde065d6c5c
R.K.Jyothish <112569842+JyothishRK@users.noreply.github.com>
2022-12-05 22:04:13 +0530

    Update MostOccuringCharacter.java

---
+++ Strings/MostOccuringCharacter.java
@@ -0,0 +1,19 @@
+package Strings;
+/* 
+Given a string S of length n and q queries. In each query, You have to find out which character occur most of the time in a given range(L, R). If there are several answers, you have to find out the lexicographically smallest letter.
+Input
+The first line will contain two integers n, q (1 ≤ n ≤ 10**5, 1 ≤ q ≤ 10**5), the length of the string and the number of queries respectively.
+The next line contains the string S.
+Next q lines will contain two integers L & R.
+(1 ≤ n ≤ 10**5, 1 ≤ q ≤ 10**5)
+Output
+For each query, find out the most occurrence character & the occurrence times. If there are several answers, you have to find out the lexicographically smallest character.
+*/
+public class MostOccuringCharacter {
+    public static void main(String[] args) {
+        
+    }
+}
+/*
+ Practice problem :)
+*/
